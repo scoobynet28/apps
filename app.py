8cc794d5a54f69fc925dfa914655193e9503bedc
scoobynet28 <77055904+scoobynet28@users.noreply.github.com>
2021-03-16 11:34:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -342,7 +342,12 @@ def main():
         scrape_comments_with_replies()
     elif activities == "Analisa Sentimen Komentar":
         st.subheader("Data Komentar YouTube")
-        df = pd.read_csv('./YouTube-Komentar.csv')
+        file_csv_yt = ('./YouTube-Komentar.csv')
+        if os.path.exists(file_csv_yt):
+                df = pd.read_csv(file_csv_yt)
+                st.dataframe(df)
+            else:
+               st.write("""Maaf Data Komentar YouTube Belum Ada""") 
 
         st.dataframe(df)
         st.write("""=========================================================================""")
