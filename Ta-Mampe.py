b91febd20e9c0aad5bf741f26f917a40963de14b
scoobynet28 <77055904+scoobynet28@users.noreply.github.com>
2021-03-16 12:31:51 +0700

    Update and rename app.py to Ta-Mampe.py

--- Ta-Mampe.py
+++ Ta-Mampe.py
@@ -13,7 +13,8 @@ from sklearn.naive_bayes import MultinomialNB
 import numpy as np
 import pickle
 import pandas as pd
-import streamlit as st 
+import streamlit as st
+
 
 def scrape_comments_with_replies():
         st.subheader("Input Video ID YouTube")
